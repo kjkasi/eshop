63882793f89722df4fd2e93527386e7abfe1bdeb
kjkasi <5842602+kjkasi@users.noreply.github.com>
2022-10-05 16:47:30 +0300

    Add CatalogContext and CatalogController

---
+++ eshop/Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CatalogItem>> GetCatalogItems()
+        {
+            Console.WriteLine("--> GetCatalogItems()");
+            return Ok();
+        }
+    }
+}
---
+++ eshop/Catalog/Models/CatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Models
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> opt)
+            : base(opt)
+        {
+        }
+    }
+}
