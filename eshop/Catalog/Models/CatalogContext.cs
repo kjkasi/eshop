a2f5efc23e61a80528df57914cf89f2067fc3496
kjkasi <5842602+kjkasi@users.noreply.github.com>
2022-10-07 14:38:57 +0300

    No message

--- eshop/Catalog/Models/CatalogContext.cs
+++ eshop/Catalog/Models/CatalogContext.cs
@@ -7,31 +7,69 @@ namespace Catalog.Models
 {
     public class CatalogContext : DbContext
     {
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
         public CatalogContext(DbContextOptions<CatalogContext> opt)
             : base(opt)
         {
+            //Database.EnsureDeleted();
             Database.EnsureCreated();
+        }
+
+        public CatalogContext()
+        {
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            #region ApplyConfiguration
             builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
-            builder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
-            builder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+            //builder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
+            //builder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+            #endregion
 
-            //CatalogBrand
+
+            #region CatalogBrandSeed
             builder.Entity<CatalogBrand>().HasData(new CatalogBrand
             {
                 Id = 1,
                 Brand = "Azure"
+            },
+            new CatalogBrand
+            {
+                Id = 2,
+                Brand = ".NET"
+            },
+            new CatalogBrand
+            {
+                Id = 3,
+                Brand = "Visual Studio"
+            },
+            new CatalogBrand
+            {
+                Id = 4,
+                Brand = "SQL Server"
+            },
+            new CatalogBrand
+            {
+                Id = 5,
+                Brand = "Other"
             });
-            //CatalogType
+            #endregion
+
+
+            #region CatalogType
             builder.Entity<CatalogType>().HasData(new CatalogType
             {
                 Id = 1,
                 Type = "Mug"
             });
-            //CatalogItem
+            #endregion
+
+            # region CatalogItem
+            /*
             builder.Entity<CatalogItem>().HasData(
             new CatalogItem
             {
@@ -54,11 +92,20 @@ namespace Catalog.Models
                 Name = ".NET Black & White Mug",
                 Price = 8.50M,
                 PictureFileName = "2.png"
+            }, 
+            new CatalogItem
+            {
+                Id = 3,
+                CatalogTypeId = 2,
+                CatalogBrandId = 5,
+                AvailableStock = 100,
+                Description = "Prism White T-Shirt",
+                Name = "Prism White T-Shirt",
+                Price = 12,
+                PictureFileName = "3.png"
             });
+            */
+            #endregion
         }
-
-        public DbSet<CatalogItem> CatalogItems { get; set; }
-        public DbSet<CatalogBrand> CatalogBrands { get; set; }
-        public DbSet<CatalogType> CatalogTypes { get; set; }
     }
 }
